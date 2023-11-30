90b0db52ddcc1b9071228992fa1751ea0d095c16
Devanshi <pdevs18@hotmail.com>
2023-11-29 21:56:54 -0600

    Added: genere selector with search and email input

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Graph from './graph.tsx';
 import CustomSlider from '../Components/CustomSlider.tsx';
+import Select, { MultiValue } from 'react-select'
+import { Input } from '@chakra-ui/react';
 
 export interface IHomeProps {
 }
@@ -8,15 +10,26 @@ export interface IHomeProps {
 export default function Home (props: IHomeProps) {
   const [danceabilityValue, setDancebilityValue] = useState(50);
   const [speechinessValue, setSpeechinessValue] = useState(50);
+  const [selectedOptions, setSelectedOptions ] = useState<MultiValue<{
+    value: string;
+    label: string;
+}>>([]);
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' }
+  ]
 
   return (
+    <div style={{flex:1}}>
     <div style={styles.parentContainer}>
-      <div style={{}}>
-        <Graph></Graph>
-      </div> 
+        <Graph/>
       <div style={styles.cotainer}>
-          <div style={{width: '100%'}}>
+          <div style={{width: '100%', display:'flex', alignItems:'start', flexDirection:'column'}}>
             <p style={{float:'left'}}>Genres</p>
+            <div style={{width: '100%', maxWidth: 400}}>
+              <Select options={options} isMulti isSearchable onChange={(newValue) => setSelectedOptions(newValue)} />
+            </div>
          </div>
           <div style={{width: '100%'}}>
             <p style={{float:'left'}}>Dancability</p>
@@ -28,23 +41,30 @@ export default function Home (props: IHomeProps) {
           </div>
         </div>
     </div>
+    <div style={{margin: '5%'}}>
+      <Input
+      style={{width: '50%', float:'left'}}
+      required
+      pr='4.5rem'
+      type={'email'}
+      placeholder='Enter Email'
+    />
+  </div>
+  </div>
   );
 }
 
 const styles = {
   parentContainer:{
     margin: '5%',
-    width: '100%',
+    flexGrow:1,
     display: 'flex',
-    justifyContent: 'space-between',
-    gap: '10%',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
     flexWrap:'wrap',
   },
   cotainer:{
-    display:'flex',
-    flex:1,
-    flexDirection:'column',
-    width: '100%',
+    width: '50%',
     justifyContent: 'space-between',
   }
 }
